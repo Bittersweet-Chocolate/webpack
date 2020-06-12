ef9787c0eaf90136b8d34121bf38584f3e54a9ac
chenzh <czh533143@163.com>
2020-06-12 22:52:09 +0800

    webpack 打包其他资源文件

--- webpack-02/webpack.config.js
+++ webpack-02/webpack.config.js
@@ -4,7 +4,7 @@
 
 const { resolve } = require("path")
 const htmlWebpackPlugin = require("html-webpack-plugin")
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -16,16 +16,30 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
+          // use 数组中loader执行顺序从下到上，从右到左执行
+          // 创建style标签，将js的样式资源插入，添加到head中生效
           "style-loader",
+          // 将css文件变成common.js模块加载到js中，为字符串
           "css-loader"
         ]
+      },
+      // 打包其他资源
+      {
+        exclude: /\.(css|js|html)$/,
+        loader: "file-loader",
+        options: {
+          // 对图片重命名，[hash:10]hash前10位 [ext]原文件拓展名
+          name: "[name]-[hash:8].min.[ext]",
+          outputPath: "static", //定义输出的图片文件夹
+        }
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new htmlWebpackPlugin({
       template: "./src/index.html"
     })
   ],
-  mode: "production"
+  mode: "development"
 }
