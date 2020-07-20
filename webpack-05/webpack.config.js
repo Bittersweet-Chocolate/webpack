da8f469355cc432cb21aeabff895c66ced1246a1
chenzh-pc <czh533143@163.com>
2020-07-20 23:07:34 +0800

    webpack-05

---
+++ webpack-05/webpack.config.js
@@ -0,0 +1,35 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/main.js',
+        path: resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+
+                    // 'style-loader',
+                    // 取代style-loader。提前js中的css成独立文件
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/mian.css'
+        })
+    ],
+    mode: 'development'
+}
